836e274b6eefd4235bb0ee39b0e75897a77277f6
Frank Rosendorf <frank.rosendorf1@gmail.com>
2019-02-11 23:52:44 -0500

    Adding additional form validation

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,8 +3,17 @@ from .models import Profile
 
 
 class ProfileForm(forms.ModelForm):
-    confirm_email = forms.EmailField()
+    confirm_email = forms.EmailField(required=False)
 
     class Meta:
         model = Profile
         fields = ['first_name', 'last_name', 'avatar', 'bio', 'date_of_birth', 'email']
+
+    def clean(self):
+        cleaned_data = super().clean()
+        email = cleaned_data.get('email', None)
+        confirm_email = cleaned_data.get('confirm_email', None)
+        # If either the email or confirm email is provided make sure they are equal
+        if email is not None or confirm_email is not None:
+            if email != confirm_email:
+                raise forms.ValidationError('Emails must match!')
