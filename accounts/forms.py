039745434443b75eb4a5dfe5e99e80d6c3b7260e
Frank Rosendorf <frank.rosendorf1@gmail.com>
2019-02-13 12:54:00 -0500

    Added different formats to date of birth

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,13 +1,26 @@
 from django import forms
 from .models import Profile
+from datetime import datetime
 
 
 class ProfileForm(forms.ModelForm):
-    confirm_email = forms.EmailField(required=False)
-
+    
     class Meta:
         model = Profile
         fields = ['first_name', 'last_name', 'avatar', 'bio', 'date_of_birth', 'email']
+
+    confirm_email = forms.EmailField(required=False)
+    date_of_birth = forms.DateField(
+        required=False,
+        widget = forms.DateInput(format='%m/%d/%Y'),
+        input_formats=('%m/%d/%Y', '%m/%d/%y', '%Y-%m-%d')
+    )
+
+    def clean_bio(self):
+        data = self.cleaned_data.get('bio', None)
+        if data and len(data) < 10:
+            raise forms.ValidationError('Bio must be at least 10 characters.')
+        return data
 
     def clean(self):
         cleaned_data = super().clean()
