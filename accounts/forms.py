fa562f94d399748a758b2eb2b1aef1e61a2d7d83
Frank Rosendorf <frank.rosendorf1@gmail.com>
2019-02-14 23:54:24 -0500

    Fixed an issue with cleaning new password field

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from .models import Profile
 
+PW_NAME_ERROR = 'Password must not include any part of your name.'
+
 
 class ProfileForm(forms.ModelForm):
     
@@ -43,12 +45,12 @@ def validate_pw_no_username(value, username):
 
 def validate_pw_no_first_name(value, first_name):
     if first_name.lower() in value.lower():
-        raise forms.ValidationError('Password must not include your first name.')
+        raise forms.ValidationError(PW_NAME_ERROR)
 
 
 def validate_pw_no_last_name(value, last_name):
     if last_name.lower() in value.lower():
-        raise forms.ValidationError('Password must not include your last name.')
+        raise forms.ValidationError(PW_NAME_ERROR)
 
 
 # New password
@@ -62,15 +64,11 @@ class ChangePasswordForm(forms.Form):
         super(ChangePasswordForm, self).__init__(*args, **kwargs)
 
     current_password = forms.CharField(widget=forms.PasswordInput())
-    new_password = forms.CharField(
-        widget=forms.PasswordInput()
-        )
-    confirm_password = forms.CharField(
-        widget=forms.PasswordInput()
-        )
+    new_password = forms.CharField(widget=forms.PasswordInput())
+    confirm_password = forms.CharField(widget=forms.PasswordInput())
 
     def clean_new_password(self):
-        new_pw = self.cleaned_data.get('new_password')
+        new_pw = self.cleaned_data['new_password']
         username = self.user.get_username()
         first_name = self.user.profile.first_name
         last_name = self.user.profile.last_name
@@ -85,8 +83,9 @@ class ChangePasswordForm(forms.Form):
         current_password = cleaned_data.get('current_password', '')
         new_password = cleaned_data.get('new_password', '')
         confirm_password = cleaned_data.get('confirm_password', '')
-        if new_password != confirm_password:
+
+        if new_password and (new_password != confirm_password):
             raise forms.ValidationError('Passwords don\'t match! Please make sure you confirm your new password.')
         
-        if current_password == new_password:
+        if new_password and (current_password == new_password):
             raise forms.ValidationError('Your new password can\'t be the same as your old password!')
